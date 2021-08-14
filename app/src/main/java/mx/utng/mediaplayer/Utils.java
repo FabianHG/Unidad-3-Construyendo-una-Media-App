72f79ce2b4ca50e81a1f5e6e0614bd309f73b1b6
FabianHG <fabian.vaio@gmail.com>
2021-08-14 18:37:53 -0500

    Se sube proyecto de TV Media Player

---
+++ app/src/main/java/mx/utng/mediaplayer/Utils.java
@@ -0,0 +1,35 @@
+package mx.utng.mediaplayer;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+    private Utils() {}
+    public static String loadJSONFromResource(Context context, int resource ) {
+        if( resource <= 0 || context == null )
+            return null;
+        String json = null;
+        InputStream is = context.getResources().openRawResource( resource );
+        try {
+            if( is != null ) {
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                json = new String(buffer, "UTF-8");
+            }
+        } catch( IOException e ) {
+            return null;
+        } finally {
+            try {
+                if( is != null )
+                    is.close();
+            } catch( IOException e ) {
+                Log.i("Error al leer", ""+e.getStackTrace());
+            }
+        }
+        return json;
+    }
+}
